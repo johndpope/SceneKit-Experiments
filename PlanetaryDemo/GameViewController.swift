05f2f07ac71e72804330fadaa0dfd835d7850f36
Jacob Martin <Jacob.Martin@HelloWorld.com>
2016-03-25 13:34:27 -0400

    ghost 2

--- PlanetaryDemo/GameViewController.swift
+++ PlanetaryDemo/GameViewController.swift
@@ -18,7 +18,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        NSInteger ghost = 1;
         // create a new scene
         let scene = SCNScene()
         
